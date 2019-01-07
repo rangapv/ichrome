15ab4f4baa50cc258c53e11fa858cf86c7627141
rangapv <rangapv@yahoo.com>
2019-01-07 06:59:50 +0000

    chorme for centos

---
+++ ichrome.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+li=$(uname -s)
+li2="${li,,}"
+
+u1=$(cat /etc/*-release | grep ubuntu)
+f1=$(cat /etc/*-release | grep ID= | grep fedora)
+c1=$(cat /etc/*-release | grep ID= | grep centos)
+s1=$(cat /etc/*-release | grep suse)
+
+
+if [ ! -z "$u1" ]
+then 
+	mi=$(lsb_release -cs)
+	mi2="${mi,,}"
+	ji=$(cat /etc/*-release | grep DISTRIB_ID | awk '{split($0,a,"=");print a[2]}')
+	ki="${ji,,}"
+
+	if [ "$ki" == "ubuntu" ]
+	then
+   	echo "IT IS UBUNTU"
+	fi
+fi
+
+
+if [ ! -z "$f1" ]
+then
+        ji=$(cat /etc/*-release | grep '^ID=' |awk '{split($0,a,"=");print a[2]}')
+        ki="${ji,,}"
+fi
+
+if [ ! -z "$c1" ]
+then
+	echo "IT IS CENTOS"
+        ji=$(cat /etc/*-release | grep '^ID=' |awk '{split($0,a,"\"");print a[2]}')
+        ki="${ji,,}"
+	sudo yum install -y  wget
+	sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+	sudo yum install -y ./google-chrome-stable_current_*.rpm
+	ln -sf /usr/bin/google-chrome-stable ./chrome
+	echo " " >> ~/.bashrc
+	echo "export PATH=$PATH:$PWD/chrome" >> ~/.bashrc
+fi
