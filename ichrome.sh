0d715542d401ba7463bffc057cd9cf77b989d05f
rangapv <rangapv@yahoo.com>
2019-01-08 08:39:22 +0000

    ubuntu / centos support added

--- ichrome.sh
+++ ichrome.sh
@@ -19,6 +19,15 @@ then
 	then
    	echo "IT IS UBUNTU"
 	fi
+        sudo apt-get -y update
+        sudo apt-get install -y wget
+        wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+        sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+        sudo apt-get -y update
+        sudo apt-get -f install
+        sudo apt-get install -y google-chrome-stable
+	ln -sf /usr/bin/google-chrome-stable $HOME/chrome
+
 fi
 
 
